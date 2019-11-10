69b0bd9dd52482ec605fb9a6435b1e63b504af59
Kirill <kkirilenkov@gmail.com>
2019-11-10 10:17:10 +0700

    other confs

--- src/main/scala/RW.scala
+++ src/main/scala/RW.scala
@@ -2,8 +2,12 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
 object RW extends App {
-    val conf = new SparkConf().setAppName("KISTLER_ANALYZER").setMaster("local[2]").set("spark.executor.memory","1g")
-    val sc = new SparkContext(conf)
+    val conf: SparkConf =
+        new SparkConf()
+          .setMaster("local[*]")
+          .setAppName("Kistler_app")
+          .set("spark.driver.host", "localhost")
+    val sc: SparkContext = new SparkContext(conf)
     val RDD = sc.textFile("C:\\Users\\Kirill\\Desktop\\kistler\\main\\Kirill 003.txt")
     RDD.collect().foreach(x => println(x))
 }
